b2186d3aacec16d9a9b40046513b0de3a58c4de0
bhall20 <brianhall401@yahoo.com>
2021-06-23 10:58:39 -0400

    Fixed books displaying bug.

--- books/models.py
+++ books/models.py
@@ -18,7 +18,6 @@ class Book(models.Model):
     url = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"Author Name ={self.author}"
 
--- books/views.py
+++ books/views.py
@@ -5,10 +5,10 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def hompage(request):
     if request.user.is_authenticated:
-        return("books_list")
-        return render(request, "books/hompage.html")
+        return("book_list")
+    return render(request, "books/homepage.html")
 
 @login_required
 def book_list(request):
-    book = Book.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-    return render(request, 'books/book_list.html', {'book': book})
+    books = Book.objects.all().order_by('created_date')
+    return render(request, 'books/book_list.html', {'books': books})
